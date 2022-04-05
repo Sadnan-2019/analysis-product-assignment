d28e860150fe8b751f09cc94188d6c37c1d22ad4
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-04-05 23:31:16 +0600

    404 img add

--- src/componenets/Error/Error.js
+++ src/componenets/Error/Error.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Error = () => {
-          return (
-                    <div>
-                           <h1>No route found 404</h1>   
-                    </div>
-          );
+  return (
+    <div className="  justify-content-center  d-flex">
+      {/* <h1>No route found 404</h1> */}
+      <img className="img-fluid mt-2"
+        src="https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Error;
